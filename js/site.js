f765c253f0f0a6f5ae88b6c1bb341645554c1a59
chrissywow <unknownchrissy@gmail.com>
2021-12-27 13:09:56 -0500

    initial commit

--- js/site.js
+++ js/site.js
@@ -1,4 +1,33 @@
-//Call Hello World
-function helloWorld(){
-    alert("hello World");
+//Get the string from the page
+function getValue(){
+
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString)
+}
+
+
+//Reverse the string
+//log function 
+function reverseString(userString){
+
+    let revString = [];
+    //reverse a string using a for loop 
+    for (let index = userString.length - 1 ; index >= 0; index --) {
+        revString += userString[index];        
+    }
+    return revString;
+}
+
+
+//Display the reversed string to the user
+//view Function
+function displayString(revString){
+
+    //write to the pages 
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+    //show the alert box 
+    document.getElementById("alert").classList.remove("invisible");
+
 }
